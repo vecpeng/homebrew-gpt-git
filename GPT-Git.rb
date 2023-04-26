785c50e0378bd5058f9965d73062b4802edc44ab
vecpeng <841298391@qq.com>
2023-04-27 00:38:45 +0800

    feat: Update GPT-Git.rb with new sha256 hash

--- GPT-Git.rb
+++ GPT-Git.rb
@@ -2,7 +2,7 @@ class GPTGit < Formula
   desc "Generate Git commit messages with ChatGPT"
   homepage "https://github.com/vecpeng/gpt-git"
   url "https://registry.npmjs.com/package/gpt-git/-/gpt-git-1.0.0.tgz"
-  sha256 "THE_SHA256_FOR_YOUR_PACKAGE"
+  sha256 "6f945a8bd178759b39098bcbf3f0c6f1501e841f258653b3e1111b06b0494e14"
   license "MIT"
 
   depends_on "node"
