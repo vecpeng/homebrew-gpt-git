b3041dbcdaad25fbd07deb82a2e2c1dc188da441
vecpeng <841298391@qq.com>
2023-04-26 23:24:10 +0800

    feat: add gpt-3 powered commit message generator from git diff

---
+++ index.js
@@ -0,0 +1,75 @@
+// git-commit.js
+import { execa } from "execa";
+import inquirer from "inquirer";
+
+const OPEN_AI_API_KEY = process.env.OPEN_AI_API_KEY;
+const OPEN_AI_API_PROXY = process.env.OPEN_AI_API_PROXY;
+// 根据 git diff --cached 获取 commit message 的函数
+async function getCommitMessagesFromDiff(diff) {
+  // 实现根据 diff 的逻辑来生成 commit message 数组
+  try {
+    const res = await fetch(`${OPEN_AI_API_PROXY ?? "https://api.openai.com"}/v1/chat/completions`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${OPEN_AI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "user",
+            content: `Please generate commit messages from the diff: ${diff}, following conventional commit (<type>: <subject>)`,
+          },
+        ],
+      }),
+    });
+    const data = await res.json();
+    if (data.error) {
+        console.error("Error requesting OpenAI API", data.error);
+        process.exit(1);
+    }
+    return data.choices[0].message.content.split('\n');
+  } catch (err) {
+    console.error("Error requesting OpenAI API", err);
+    process.exit(1);
+  }
+}
+
+async function getGitDiff() {
+  try {
+    const { stdout } = await execa("git", ["diff", "--cached"]);
+    return stdout;
+  } catch (error) {
+    console.error("Error getting git diff");
+    process.exit(1);
+  }
+}
+
+async function commitWithMessage(message) {
+  try {
+    await execa("git", ["commit", "-m", message]);
+    console.log("Commit successful");
+  } catch (error) {
+    console.error("Error committing changes");
+    process.exit(1);
+  }
+}
+
+async function main() {
+  const gitDiff = await getGitDiff();
+  const commitMessages = await getCommitMessagesFromDiff(gitDiff);
+  console.log(commitMessages);
+  const { selectedMessage } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "selectedMessage",
+      message: "Select a commit message:",
+      choices: commitMessages,
+    },
+  ]);
+
+  await commitWithMessage(selectedMessage);
+}
+
+main();
