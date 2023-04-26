4ffbc9e150f908deace34c1c5ed8983f11dc058f
vecpeng <841298391@qq.com>
2023-04-27 00:52:10 +0800

    fix: brew name

--- formula/gpt-git.rb
+++ formula/gpt-git.rb
@@ -1,7 +1,7 @@
 class GptGit < Formula
   desc "Generate Git commit messages with ChatGPT"
   homepage "https://github.com/vecpeng/gpt-git"
-  url "https://registry.npmjs.com/package/gpt-git/-/gpt-git-1.0.0.tgz"
+  url "https://registry.npmjs.com/gpt-git/-/gpt-git-1.0.0.tgz"
   sha256 "6f945a8bd178759b39098bcbf3f0c6f1501e841f258653b3e1111b06b0494e14"
   license "MIT"
 
